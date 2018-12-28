c4412fbeb1783beb7325a3f4e51a895dcd4fdb8d
Ieuan Lovett <ieuan.lovett@gmail.com>
2018-12-28 13:05:04 +1300

    simplify tags

--- banana_py/templatetags/banana_tags.py
+++ banana_py/templatetags/banana_tags.py
@@ -6,9 +6,5 @@ from banana_py import Bananas_OAuth
 register = template.Library()
 
 @register.simple_tag
-def banana_auth_url():
-    return Bananas_OAuth().authorize_url()
-
-@register.simple_tag
-def banana_auth_link(link_text):
-    return f'<a href="{banana_auth_url()}">{link_text}</a>'
+def banana_auth_url(link_text):
+    return format_html("<a href=\"{}\">{}</a>", Bananas_OAuth().authorize_url(), link_text)
